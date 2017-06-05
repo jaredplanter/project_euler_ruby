7069a31f14b157520dadbd3d3c94d2990569e9b5
Jared Planter <jaredplanter@gmail.com>
2017-06-04 17:03:05 -0700

    Add test for Problem3 class

--- test/euler_test.rb
+++ test/euler_test.rb
@@ -8,4 +8,8 @@ class EulerTest < Minitest::Test
   def test_that_problem_1_gets_the_right_answer
     assert_equal 233168, Euler::Problem1.solve
   end
+
+  def test_that_problem_3_gets_the_right_answer
+    assert_equal 6857, Euler::Problem3.solve
+  end
 end
