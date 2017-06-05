4b9677ecf213b5197039a8a440500befaa1e8ffe
Jared Planter <jaredplanter@gmail.com>
2017-06-04 17:03:50 -0700

    Add test for Problem5 class

--- test/euler_test.rb
+++ test/euler_test.rb
@@ -12,4 +12,8 @@ class EulerTest < Minitest::Test
   def test_that_problem_3_gets_the_right_answer
     assert_equal 6857, Euler::Problem3.solve
   end
+
+  def test_that_problem_5_gets_the_right_answer
+    assert_equal 232792560, Euler::Problem5.solve
+  end
 end
