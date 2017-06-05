fa4be4d8d6f95444332a0f095fafa7b56b106000
Jared Planter <jaredplanter@gmail.com>
2017-06-04 17:27:01 -0700

    Add code to display all problems and their respective solution

--- lib/euler.rb
+++ lib/euler.rb
@@ -1,2 +1,11 @@
 module Euler
+  Dir['./lib/euler/problem*.rb'].each do |file|
+    require file
+    problem = "#{File.basename(file, ".rb").capitalize}"
+    klass_name = "Euler::#{problem}"
+    klass = Object.const_get klass_name
+    print "_#{problem}_\n"
+    print "Question: #{klass.question}\n"
+    print "Answer: #{klass.solve}\n\n"
+  end
 end
